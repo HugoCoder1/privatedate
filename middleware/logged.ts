653c900bff08059b5b95cd770575d96dae0eee09
Hugues <hugueshugues2002@gmail.com>
2024-07-13 21:08:36 +0100

    update middleware

--- middleware/logged.ts
+++ middleware/logged.ts
@@ -2,11 +2,6 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   const user = await getCurrentUser();
 
   if (user) {
-    return await navigateTo({
-      path: "/user/home",
-      query: {
-        redirect: to.fullPath,
-      },
-    });
+    return await navigateTo("/user/home");
   }
 });
